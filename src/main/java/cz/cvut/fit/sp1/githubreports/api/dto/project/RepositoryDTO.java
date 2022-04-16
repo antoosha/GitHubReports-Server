0e0a04dfcc4bbd5547e8a78db3bb7fee911086c5
Anton Korolov <akorol6969@gmail.com>
2022-04-16 18:55:25 +0200

    add DTOs implementations

--- src/main/java/cz/cvut/fit/sp1/githubreports/api/dto/project/RepositoryDTO.java
+++ src/main/java/cz/cvut/fit/sp1/githubreports/api/dto/project/RepositoryDTO.java
@@ -1,4 +1,21 @@
 package cz.cvut.fit.sp1.githubreports.api.dto.project;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RepositoryDTO {
+
+    private Long repositoryID;
+    private String repositoryName;
+    private List<Long> commitsIDs;
+    private List<Long> projectsIDs;
+
 }
