da32b837da36d8d58d0518cec3a9da60a022f171
Nikita Savchenko <savchnikn@fit.cvut.cz>
2022-04-22 20:09:34 +0200

    Add Role and User converters.

--- src/main/java/cz/cvut/fit/sp1/githubreports/service/user/role/RoleConverter.java
+++ src/main/java/cz/cvut/fit/sp1/githubreports/service/user/role/RoleConverter.java
@@ -1,4 +1,43 @@
 package cz.cvut.fit.sp1.githubreports.service.user.role;
 
+import cz.cvut.fit.sp1.githubreports.api.dto.user.RoleDTO;
+import cz.cvut.fit.sp1.githubreports.model.user.Role;
+import cz.cvut.fit.sp1.githubreports.model.user.User;
+import cz.cvut.fit.sp1.githubreports.service.user.user.UserService;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Component
 public class RoleConverter {
+
+    private final UserService userService;
+
+    public RoleConverter(UserService userService) {
+        this.userService = userService;
+    }
+
+    public Role toModel(RoleDTO roleDTO) {
+        return new Role(
+                roleDTO.getRoleName(),
+                roleDTO.getUsersIDs().stream().map(userService::getUserById).collect(Collectors.toList())
+        );
+    }
+
+    public RoleDTO fromModel(Role role) {
+        return new RoleDTO(
+                role.getRoleName(),
+                role.getUsers().stream().map(User::getUserId).collect(Collectors.toList())
+        );
+    }
+
+    public Collection<Role> toModelsMany(Collection<RoleDTO> userDTOS) {
+        return userDTOS.stream().map(this::toModel).collect(Collectors.toList());
+    }
+
+    public Collection<RoleDTO> fromModelsMany(Collection<Role> roles) {
+        return roles.stream().map(this::fromModel).collect(Collectors.toList());
+    }
+
 }
--- src/main/java/cz/cvut/fit/sp1/githubreports/service/user/user/UserConverter.java
+++ src/main/java/cz/cvut/fit/sp1/githubreports/service/user/user/UserConverter.java
@@ -1,3 +1,68 @@
 package cz.cvut.fit.sp1.githubreports.service.user.user;
+
+import cz.cvut.fit.sp1.githubreports.api.dto.user.UserDTO;
+import cz.cvut.fit.sp1.githubreports.model.project.Comment;
+import cz.cvut.fit.sp1.githubreports.model.project.Project;
+import cz.cvut.fit.sp1.githubreports.model.statistic.Statistic;
+import cz.cvut.fit.sp1.githubreports.model.user.Role;
+import cz.cvut.fit.sp1.githubreports.model.user.User;
+import cz.cvut.fit.sp1.githubreports.service.project.comment.CommentService;
+import cz.cvut.fit.sp1.githubreports.service.project.project.ProjectService;
+import cz.cvut.fit.sp1.githubreports.service.statistic.statistic.StatisticService;
+import cz.cvut.fit.sp1.githubreports.service.user.role.RoleService;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Component
 public class UserConverter {
+
+    private final CommentService commentService;
+    private final ProjectService projectService;
+    private final StatisticService statisticService;
+    private final RoleService roleService;
+
+    public UserConverter(CommentService commentService, ProjectService projectService, StatisticService statisticService, RoleService roleService) {
+        this.commentService = commentService;
+        this.projectService = projectService;
+        this.statisticService = statisticService;
+        this.roleService = roleService;
+    }
+
+    public User toModel(UserDTO userDTO) {
+        return new User(
+                userDTO.getUserId(),
+                userDTO.getUsername(),
+                userDTO.getEmail(),
+                userDTO.getPassword(),
+                userDTO.getPathToFileWithPhoto(),
+                userDTO.getCommentsIDs().stream().map(commentService::getCommentById).collect(Collectors.toList()),
+                userDTO.getProjectsIDs().stream().map(projectService::getProjectById).collect(Collectors.toList()),
+                userDTO.getStatisticsIDs().stream().map(statisticServicee::getStatisticById).collect(Collectors.toList()),
+                userDTO.getRolesIDs().stream().map(roleService::getRolesById).collect(Collectors.toList())
+                );
+    }
+
+    public UserDTO fromModel(User user) {
+        return new UserDTO(
+                user.getUserId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getPathToFileWithPhoto(),
+                user.getComments().stream().map(Comment::getCommentId).collect(Collectors.toList()),
+                user.getProjects().stream().map(Project::getProjectId).collect(Collectors.toList()),
+                user.getStatistics().stream().map(Statistic::getStatisticId).collect(Collectors.toList()),
+                user.getRoles().stream().map(Role::getRoleName).collect(Collectors.toList())
+                );
+    }
+
+    public Collection<User> toModelsMany(Collection<UserDTO> userDTOS) {
+        return userDTOS.stream().map(this::toModel).collect(Collectors.toList());
+    }
+
+    public Collection<UserDTO> fromModelsMany(Collection<User> users) {
+        return users.stream().map(this::fromModel).collect(Collectors.toList());
+    }
 }
