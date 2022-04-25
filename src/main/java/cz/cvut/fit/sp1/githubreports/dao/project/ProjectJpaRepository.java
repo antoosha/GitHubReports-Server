25eae187d38cf42ff6fd238787115288f6354be5
Anton Korolov <akorol6969@gmail.com>
2022-04-25 22:09:02 +0200

    add project and repository services, converters, SPIs and JpaRepositories. Some implementation for the second iteration.
    TODO: change RuntimeException to correct exception

--- src/main/java/cz/cvut/fit/sp1/githubreports/dao/project/ProjectJpaRepository.java
+++ src/main/java/cz/cvut/fit/sp1/githubreports/dao/project/ProjectJpaRepository.java
@@ -1,4 +1,9 @@
 package cz.cvut.fit.sp1.githubreports.dao.project;
 
-public interface ProjectJpaRepository {
+import cz.cvut.fit.sp1.githubreports.model.project.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectJpaRepository extends JpaRepository<Project, Long> {
 }
