d65949b203953bb5653967b1386b729ea30c0b40
Anna <churiann@fiit.cvut.cz>
2022-05-04 20:24:29 +0200

    add implementation of StatisticController and StatisticTypeController change methods update and create in StatisticService, StatisticSPI, StatisticTypeService, StatisticTypeSPI

--- src/main/java/cz/cvut/fit/sp1/githubreports/api/controller/statistic/StatisticTypeController.java
+++ src/main/java/cz/cvut/fit/sp1/githubreports/api/controller/statistic/StatisticTypeController.java
@@ -1,4 +1,54 @@
 package cz.cvut.fit.sp1.githubreports.api.controller.statistic;
 
+import cz.cvut.fit.sp1.githubreports.api.dto.statistic.StatisticTypeDTO;
+import cz.cvut.fit.sp1.githubreports.api.exceptions.EntityStateException;
+import cz.cvut.fit.sp1.githubreports.api.exceptions.IncorrectRequestException;
+import cz.cvut.fit.sp1.githubreports.api.exceptions.NoEntityFoundException;
+import cz.cvut.fit.sp1.githubreports.service.statistic.statisticType.StatisticTypeConverter;
+import cz.cvut.fit.sp1.githubreports.service.statistic.statisticType.StatisticTypeSPI;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/statisticTypes")
 public class StatisticTypeController {
+
+    private final StatisticTypeSPI statisticTypeSPI;
+    private final StatisticTypeConverter statisticTypeConverter;
+
+    @GetMapping()
+    public Collection<StatisticTypeDTO> getAll() {
+        return statisticTypeConverter.fromModelsMany(statisticTypeSPI.readAll());
+    }
+
+    @GetMapping("/{id}")
+    public StatisticTypeDTO getOne(@PathVariable String id) {
+        return statisticTypeConverter.fromModel(statisticTypeSPI.readById(id).orElseThrow(NoEntityFoundException::new));
+    }
+
+    @PostMapping()
+    public StatisticTypeDTO create(StatisticTypeDTO statisticTypeDTO) throws EntityStateException {
+        return statisticTypeConverter.fromModel(statisticTypeSPI.create(statisticTypeConverter.toModel(statisticTypeDTO)));
+    }
+
+    @PutMapping("/{id}")
+    public StatisticTypeDTO update(@PathVariable String id, @RequestBody StatisticTypeDTO statisticTypeDTO) throws IncorrectRequestException, EntityStateException {
+        if (!statisticTypeDTO.getStatisticName().equals(id)) {
+            throw new IncorrectRequestException();
+        }
+        return statisticTypeConverter.fromModel(statisticTypeSPI.update(statisticTypeDTO.getStatisticName(), statisticTypeConverter.toModel(statisticTypeDTO)));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        if (statisticTypeSPI.readById(id).isEmpty()) {
+            throw new NoEntityFoundException();
+        }
+        statisticTypeSPI.delete(id);
+    }
+
+
 }
