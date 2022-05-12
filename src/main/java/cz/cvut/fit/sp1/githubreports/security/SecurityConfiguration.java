8f1f351c354fc2b3ba988ce3a55c1c6d30c5c5b6
Kirill Ponomarov <kirill.ponomarov.work@gmail.com>
2022-05-12 11:17:51 +0200

    CORS update (all headers are allowed);

--- src/main/java/cz/cvut/fit/sp1/githubreports/security/SecurityConfiguration.java
+++ src/main/java/cz/cvut/fit/sp1/githubreports/security/SecurityConfiguration.java
@@ -75,6 +75,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
