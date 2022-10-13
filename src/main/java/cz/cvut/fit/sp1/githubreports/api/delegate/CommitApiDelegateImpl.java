46aed53e2961eddf22d7a179f62db17dc0a94cce
Kirill Alekhnovich <kirillalekhnovich@gmail.com>
2022-10-13 21:03:21 +0200

    Commits delegate controller and service implemented

---
+++ src/main/java/cz/cvut/fit/sp1/githubreports/api/delegate/CommitApiDelegateImpl.java
@@ -0,0 +1,58 @@
+package cz.cvut.fit.sp1.githubreports.api.delegate;
+
+import cz.cvut.fit.sp1.githubreports.api.exceptions.NoEntityFoundException;
+import cz.cvut.fit.sp1.githubreports.service.project.commit.CommitMapper;
+import cz.cvut.fit.sp1.githubreports.service.project.commit.CommitSPI;
+import lombok.RequiredArgsConstructor;
+import org.openapi.api.CommitsApi;
+import org.openapi.model.CommentUpdateSlimDTO;
+import org.openapi.model.CommitDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+public class CommitApiDelegateImpl implements CommitsApi {
+
+    private final CommitSPI commitSPI;
+
+    private final CommitMapper commitMapper;
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return CommitsApi.super.getRequest();
+    }
+
+    @Override
+    public ResponseEntity<CommitDTO> addComment(Long id, CommentUpdateSlimDTO commentUpdateSlimDTO) {
+        CommitDTO commitDTO = commitMapper.toDTO(commitSPI.addComment(id, commentUpdateSlimDTO));
+        return ResponseEntity.ok(commitDTO);
+    }
+
+    @Override
+    public ResponseEntity<CommitDTO> addTag(Long id, Long tagId) {
+        CommitDTO commitDTO = commitMapper.toDTO(commitSPI.addTag(id, tagId));
+        return ResponseEntity.ok(commitDTO);
+    }
+
+    @Override
+    public ResponseEntity<CommitDTO> deleteComment(Long id, Long commentId) {
+        CommitDTO commitDTO = commitMapper.toDTO(commitSPI.deleteComment(id, commentId));
+        return ResponseEntity.ok(commitDTO);
+    }
+
+    @Override
+    public ResponseEntity<CommitDTO> getCommit(Long id) {
+        CommitDTO commitDTO = commitMapper.toDTO(commitSPI.readById(id).orElseThrow(NoEntityFoundException::new));
+        return ResponseEntity.ok(commitDTO);
+    }
+
+    @Override
+    public ResponseEntity<CommitDTO> removeTag(Long id, Long tagId) {
+        CommitDTO commitDTO = commitMapper.toDTO(commitSPI.removeTag(id, tagId));
+        return ResponseEntity.ok(commitDTO);
+    }
+}
--- src/main/java/cz/cvut/fit/sp1/githubreports/service/project/commit/CommitService.java
+++ src/main/java/cz/cvut/fit/sp1/githubreports/service/project/commit/CommitService.java
@@ -1,20 +1,46 @@
 package cz.cvut.fit.sp1.githubreports.service.project.commit;
 
 import cz.cvut.fit.sp1.githubreports.api.exceptions.EntityStateException;
+import cz.cvut.fit.sp1.githubreports.api.exceptions.IncorrectRequestException;
 import cz.cvut.fit.sp1.githubreports.api.exceptions.NoEntityFoundException;
+import cz.cvut.fit.sp1.githubreports.dao.project.CommentJpaRepository;
 import cz.cvut.fit.sp1.githubreports.dao.project.CommitJpaRepository;
+import cz.cvut.fit.sp1.githubreports.dao.project.TagJpaRepository;
+import cz.cvut.fit.sp1.githubreports.dao.user.UserJpaRepository;
+import cz.cvut.fit.sp1.githubreports.model.project.Comment;
 import cz.cvut.fit.sp1.githubreports.model.project.Commit;
+import cz.cvut.fit.sp1.githubreports.model.project.Tag;
+import cz.cvut.fit.sp1.githubreports.service.project.comment.CommentMapper;
+import cz.cvut.fit.sp1.githubreports.service.project.comment.CommentService;
+import cz.cvut.fit.sp1.githubreports.service.project.tag.TagMapper;
+import cz.cvut.fit.sp1.githubreports.service.user.user.UserService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.openapi.model.CommentUpdateSlimDTO;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Optional;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
+@Transactional
 @Service("CommitService")
 public class CommitService implements CommitSPI {
 
-    CommitJpaRepository repository;
+    private final CommitJpaRepository commitJpaRepository;
+
+    private final CommentJpaRepository commentJpaRepository;
+
+    private final TagJpaRepository tagJpaRepository;
+
+    private final CommentMapper commentMapper;
+
+    private final CommentService commentService;
+
+    private final UserJpaRepository userJpaRepository;
+
 
     private void checkValidation(Commit commit) {
         if (commit.getRepository() == null)
@@ -24,42 +50,85 @@ public class CommitService implements CommitSPI {
 
     @Override
     public Collection<Commit> readAll() {
-        return repository.findAll();
+        return commitJpaRepository.findAll();
     }
 
     @Override
     public Optional<Commit> readById(Long id) {
-        return repository.findById(id);
+        return commitJpaRepository.findById(id);
     }
 
     @Override
     public Commit create(Commit commit) throws EntityStateException {
         if (commit.getCommitId() != null) {
-            if (repository.existsById(commit.getCommitId()))
+            if (commitJpaRepository.existsById(commit.getCommitId()))
                 throw new EntityStateException();
         }
         checkValidation(commit);
-        return repository.save(commit);
+        return commitJpaRepository.save(commit);
     }
 
     @Override
     public Commit update(Long id, Commit commit) throws EntityStateException {
-        if (id == null || !repository.existsById(id))
+        if (id == null || !commitJpaRepository.existsById(id))
             throw new NoEntityFoundException();
         checkValidation(commit);
-        return repository.save(commit);
+        return commitJpaRepository.save(commit);
     }
 
     @Override
     public void delete(Long id) {
-        if (repository.existsById(id))
-            repository.deleteById(id);
+        if (commitJpaRepository.existsById(id))
+            commitJpaRepository.deleteById(id);
         else throw new NoEntityFoundException();
     }
 
     @Override
     public void changeIsDeleted(Commit commit, boolean isDeleted) {
         commit.setDeleted(isDeleted);
-        repository.save(commit);
+        commitJpaRepository.save(commit);
+    }
+
+    @Override
+    public Commit addComment(Long id, CommentUpdateSlimDTO commentUpdateSlimDTO) {
+        Commit commit = commitJpaRepository.findById(id).orElseThrow(NoEntityFoundException::new);
+        Comment comment = commentMapper.fromUpdateSlimDTO(commentUpdateSlimDTO);
+        comment.setAuthor(userJpaRepository.findUserByUsername(commit.getLoginAuthor()).orElseThrow(NoEntityFoundException::new));
+        commentService.create(comment);
+        commit.getComments().add(comment);
+        return commitJpaRepository.save(commit);
+    }
+
+    @Override
+    public Commit addTag(Long id, Long tagId) {
+        Commit commit = commitJpaRepository.findById(id).orElseThrow(NoEntityFoundException::new);
+        Tag tag = tagJpaRepository.findById(tagId).orElseThrow(NoEntityFoundException::new);
+        if (commit.getTags().contains(tag)) {
+            throw new IncorrectRequestException();
+        }
+        commit.getTags().add(tag);
+        return commitJpaRepository.save(commit);
+    }
+
+    @Override
+    public Commit deleteComment(Long id, Long commentId) {
+        Commit commit = commitJpaRepository.findById(id).orElseThrow(NoEntityFoundException::new);
+        Comment comment = commentJpaRepository.findById(commentId).orElseThrow(NoEntityFoundException::new);
+        if (!commit.getComments().contains(comment)) {
+            throw new IncorrectRequestException();
+        }
+        commit.getComments().remove(comment);
+        return commitJpaRepository.save(commit);
+    }
+
+    @Override
+    public Commit removeTag(Long id, Long tagId) {
+        Commit commit = commitJpaRepository.findById(id).orElseThrow(NoEntityFoundException::new);
+        Tag tag = tagJpaRepository.findById(tagId).orElseThrow(NoEntityFoundException::new);
+        if (!commit.getTags().contains(tag)) {
+            throw new IncorrectRequestException();
+        }
+        commit.getTags().remove(tag);
+        return commitJpaRepository.save(commit);
     }
 }
