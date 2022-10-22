099bc7fe885b58e93c8dbb834bce91e75d7c687c
Anna Churina <churiann@fit.cvut.cz>
2022-10-22 15:13:39 +0200

    add logs to tag, comment and commit

--- src/main/java/cz/cvut/fit/sp1/githubreports/service/project/comment/CommentService.java
+++ src/main/java/cz/cvut/fit/sp1/githubreports/service/project/comment/CommentService.java
@@ -9,12 +9,15 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.logging.Logger;
 
 @AllArgsConstructor
 @Service("CommentService")
 public class CommentService implements CommentSPI {
 
     private final CommentJpaRepository repository;
+
+    private static final Logger logger = Logger.getLogger(CommentService.class.getName());
 
     @Override
     public Collection<Comment> readAll() {
@@ -25,13 +28,18 @@ public class CommentService implements CommentSPI {
     public Comment readById(Long id) {
         return repository
                 .findById(id)
-                .orElseThrow(() -> new NoEntityFoundException("Comment with id " + id + " does not exist."));
+                .orElseThrow(() -> {
+                    logger.warning("Comment with id " + id + " does not exist.");
+                    return new NoEntityFoundException("Comment with id " + id + " does not exist.");
+                });
     }
 
     @Override
     public Comment create(Comment comment) throws EntityStateException {
-        if (comment.getCommentId() != null && repository.existsById(comment.getCommentId()))
+        if (comment.getCommentId() != null && repository.existsById(comment.getCommentId())){
+            logger.warning("Comment with id " + comment.getCommentId() + " already exists.");
             throw new EntityStateException("Comment with id " + comment.getCommentId() + " already exists.");
+        }
         comment.setCreatedDate(LocalDateTime.now());
         comment.setAuthorUsername(comment.getAuthor().getUsername());
         return repository.save(comment);
@@ -42,7 +50,6 @@ public class CommentService implements CommentSPI {
         Comment comment = readById(id);
         comment.setText(commentToUpdate.getText());
         comment.setIsEdited(true);
-
         return repository.save(comment);
     }
 
@@ -50,6 +57,9 @@ public class CommentService implements CommentSPI {
     public void delete(Long id) {
         if (repository.existsById(id))
             repository.deleteById(id);
-        else throw new NoEntityFoundException("Comment with id " + id + " does not exist.");
+        else {
+            logger.warning("Comment with id " + id + " does not exist.");
+            throw new NoEntityFoundException("Comment with id " + id + " does not exist.");
+        }
     }
 }
--- src/main/java/cz/cvut/fit/sp1/githubreports/service/project/tag/TagService.java
+++ src/main/java/cz/cvut/fit/sp1/githubreports/service/project/tag/TagService.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.logging.Logger;
 
 @AllArgsConstructor
 @Service("TagService")
@@ -15,22 +16,32 @@ public class TagService implements TagSPI {
 
     TagJpaRepository repository;
 
+    private static final Logger logger = Logger.getLogger(TagService.class.getName());
+
     private void checkValidation(Tag tag) {
         if (tag.getProject() == null)
             throw new EntityStateException("Tag with id " + tag.getTagId() + " does not have project.");
         if (tag.getTagId() != null) {
             if (!tag.getTagName().equals(this.readById(tag.getTagId()).getTagName()))
                 if (tag.getProject().getTags().stream()
-                        .anyMatch(tag1 -> tag1.getTagName().equals(tag.getTagName())))
+                        .anyMatch(tag1 -> tag1.getTagName().equals(tag.getTagName()))){
+                    logger.warning("Project with id " + tag.getProject().getProjectId()
+                            + " already has tag with name " + tag.getTagName());
                     throw new EntityStateException
                             ("Project with id " + tag.getProject().getProjectId()
                                     + " already has tag with name " + tag.getTagName());
+                }
+
         } else {
             if (tag.getProject().getTags().stream()
-                    .anyMatch(tag1 -> tag1.getTagName().equals(tag.getTagName())))
+                    .anyMatch(tag1 -> tag1.getTagName().equals(tag.getTagName()))){
+                logger.warning("Project with id " + tag.getProject().getProjectId()
+                        + " already has tag with name " + tag.getTagName());
                 throw new EntityStateException
                         ("Project with id " + tag.getProject().getProjectId()
                                 + " already has tag with name " + tag.getTagName());
+            }
+
         }
     }
 
@@ -43,13 +54,17 @@ public class TagService implements TagSPI {
     public Tag readById(Long id) {
         return repository
                 .findById(id)
-                .orElseThrow(() -> new NoEntityFoundException("Tag with id " + id + " does not exist."));
+                .orElseThrow(() -> {
+                    logger.warning("Tag with id " + id + " does not exist.");
+                    return new NoEntityFoundException("Tag with id " + id + " does not exist.");
+                });
     }
 
     @Override
     public Tag create(Tag tag) throws EntityStateException {
         if (tag.getTagId() != null && repository.existsById(tag.getTagId())) {
-                throw new EntityStateException("Tag with id " + tag.getTagId() + " does not exist.");
+            logger.warning("Tag with id " + tag.getTagId() + " does not exist.");
+            throw new EntityStateException("Tag with id " + tag.getTagId() + " does not exist.");
         }
         checkValidation(tag);
         return repository.save(tag);
@@ -57,8 +72,10 @@ public class TagService implements TagSPI {
 
     @Override
     public Tag update(Long id, Tag tag) throws EntityStateException {
-        if (id == null || !repository.existsById(id))
+        if (id == null || !repository.existsById(id)){
+            logger.warning("Tag with id " + id + " does not exist.");
             throw new NoEntityFoundException("Tag with id " + id + " does not exist.");
+        }
         checkValidation(tag);
         return repository.save(tag);
     }
@@ -67,6 +84,9 @@ public class TagService implements TagSPI {
     public void delete(Long id) {
         if (repository.existsById(id))
             repository.deleteById(id);
-        else throw new NoEntityFoundException("Tag with id " + id + " does not exist.");
+        else {
+            logger.warning("Tag with id " + id + " does not exist.");
+            throw new NoEntityFoundException("Tag with id " + id + " does not exist.");
+        }
     }
 }
